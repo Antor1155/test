f1aaa50f45133a14c7e92ed4b143ced7b3fc7c86
antor <md.antor1155@gmail.com>
2023-05-07 19:01:02 +0600

    collected to mongodb server

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
-const {MongoClicne, objectId} = require('mongodb')
+const {MongoClient, objectId} = require('mongodb')
 
-require('dotenv')
+
+require('dotenv').config()
+
+//declearing mongodb
+const userName = process.env.user
+const passWord = process.env.pass
+
+const url = `mongodb+srv://${userName}:${passWord}@cluster0.js1z3.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(url)
+
 
 const cors = require("cors")
 
@@ -15,10 +24,17 @@ app.use(express.json())
 
 async function run(){
     try{
+        await client.connect()
+        const productCollection = client.db("warehouseOfFoodCompany").collection("Products")
+
         app.get("/", async(req, res) =>{
             let text = "test is going online"
+            const query = {};
+            const cursor =  productCollection.find(query)
+            const products = await cursor.limit(6).toArray()
+            // console.log(products)
 
-            res.send(text)
+            res.send(products)
         })
 
         app.get("/home", async(req, res) =>{
@@ -32,7 +48,7 @@ async function run(){
     }
 }
 
-run()
+run().catch(console.dir)
 
 app.listen(port, ()=>{
     console.log("port is :", port)
