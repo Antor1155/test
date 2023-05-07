700a86e5819734fa7ebcaef10e668ff1eae0df95
antor <md.antor1155@gmail.com>
2023-05-07 20:37:16 +0600

    added url to uri

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express')
-
 const app = express()
-
 const port = process.env.PORT || 5000
-
-const {MongoClient, objectId} = require('mongodb')
+const {MongoClient} = require('mongodb')
 
 
 require('dotenv').config()
@@ -12,10 +9,9 @@ require('dotenv').config()
 //declearing mongodb
 const userName = process.env.user
 const passWord = process.env.pass
-console.log("username and passwords are: ", userName, passWord)
 
-const url = `mongodb+srv://${userName}:${passWord}@cluster0.js1z3.mongodb.net/?retryWrites=true&w=majority`
-const client = new MongoClient(url)
+const uri = `mongodb+srv://${userName}:${passWord}@cluster0.js1z3.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri)
 
 
 const cors = require("cors")
@@ -25,18 +21,16 @@ app.use(express.json())
 
 async function run(){
     try{
-        // await client.connect()
-        // const productCollection = client.db("warehouseOfFoodCompany").collection("Products")
+        await client.connect()
+        const productCollection = client.db("warehouseOfFoodCompany").collection("Products")
 
         app.get("/", async(req, res) =>{
-            let products = ["hello", "how", "are", "you"]
-            // const query = {};
-            // const cursor =  productCollection.find(query)
-            // let products = await cursor.limit(6).toArray()
+            // let products = ["hello", "how", "are", "you"]
+            const query = {};
+            const cursor =  productCollection.find(query)
+            let products = await cursor.limit(6).toArray()
 
-            console.log(typeof products)
-            products = JSON.stringify(products)
-            console.log(typeof products)
+
             res.send(products)
         })
 
