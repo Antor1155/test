d6d4cbb91f8ead17e8932cfdf041b957a03cb6f0
antor <md.antor1155@gmail.com>
2023-05-07 20:46:40 +0600

    added new function client.connect

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const {MongoClient} = require('mongodb')
+const { MongoClient } = require('mongodb')
 
 
 require('dotenv').config()
@@ -19,40 +19,34 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 
-async function run(){
-    try{
-        await client.connect()
-        const productCollection = client.db("warehouseOfFoodCompany").collection("Products")
 
-        app.get("/", async(req, res) =>{
-            // let products = ["hello", "how", "are", "you"]
-            const query = {};
-            const cursor =  productCollection.find(query)
-            let products = await cursor.limit(6).toArray()
+const productCollection = client.db("warehouseOfFoodCompany").collection("Products")
+
+app.get("/", async (req, res) => {
+    // let products = ["hello", "how", "are", "you"]
+    const query = {}
+    const cursor = productCollection.find(query)
+    let products = await cursor.limit(6).toArray()
 
 
-            res.send(products)
-        })
+    res.send(products)
+})
 
-        app.get("/home", async(req, res) =>{
-            let text ="text is :" + userName + passWord
+app.get("/home", async (req, res) => {
+    let text = "text is :" + userName + passWord
 
-            res.send(text)
-        })
-        app.get("/main", async(req, res) =>{
-            let text ="text is : form main"
+    res.send(text)
+})
+app.get("/main", async (req, res) => {
+    let text = "text is : form main"
 
-            res.send(text)
-        })
-    }
-    catch(error){console.log(error)}
-    finally{
+    res.send(text)
+})
 
-    }
-}
 
-run().catch(console.dir)
-
-app.listen(port, ()=>{
-    console.log("port is :", port)
+client.connect(err => {
+    if(err){ console.error(err)}
+    app.listen(port, () => {
+        console.log("port is :", port)
+    })
 })
